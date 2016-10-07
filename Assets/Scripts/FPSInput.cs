7d490cb813c7d28fa432a6babbdccbd49f0e0be7
Peter Wages <Peter Wages>
2016-10-06 22:19:45 -0400

    Initial commit - Maze code base

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -.8f;
+
+    private CharacterController charController;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        float vertSpeed = 0;
+        float jumpSpeed = 100f;
+        if (charController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed += gravity;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity;
+        }
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = vertSpeed;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+
+    }
+}
