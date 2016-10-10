cfd9b04fa42a1d070d31da026b7fcb61e64d3634
James Greenwell <jameszach@jamess-mbp-7.elon.edu>
2016-10-10 19:17:46 -0400

    added some enemy script

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
-
+	private int health = 50;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnCollisionEnter(Collision coll){
+		GameObject collidedWith = coll.gameObject;
+		// This determines whether or not the enemy was hit by a bullet
+		if (collidedWith.tag == "Bullet") {
+		}
 	}
 }
