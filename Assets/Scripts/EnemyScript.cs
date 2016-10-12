156c490ba24458f2a216f52362d13085daf1e09e
James Greenwell <jameszach@jamess-mbp-7.elon.edu>
2016-10-12 02:16:22 -0400

    added a lot of enemy functionality

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,11 +8,18 @@ using System.Collections;
 */ 
 
 public class EnemyScript : MonoBehaviour {
+	//used to set the health an enemy has
 	[SerializeField] private int health = 1;
+
+	//default x and z velocity
 	[SerializeField] private float xVel = 0;
 	[SerializeField] private float zVel = 0;
+
+	//default patrolling distance
 	[SerializeField] private float distance = 0;
 
+	[SerializeField] private Transform playerT;
+	//current distance traveled on patrolling path
 	private float currentDistance = 0;
 
 	//these two variables make it possible to set negative velocities without having to worry about it affecting 
@@ -26,10 +33,15 @@ public class EnemyScript : MonoBehaviour {
 	//to simulate gradual turning for enemies
 	private bool rotating = false;
 	private int rotated = 0;
+
+	//used to save patroling angle
+	private float angleBeforeEngage;
+
 	// Use this for initialization
 	void Start () {
 		adjX = Mathf.Abs (xVel);
 		adjZ = Mathf.Abs (zVel);
+		angleBeforeEngage = this.transform.rotation.y;
 	}
 	
 	// Responsible for enemy movement
@@ -67,8 +79,6 @@ public class EnemyScript : MonoBehaviour {
 					rotating = false;
 				}
 			}
-		} else {
-			EngagePlayer ();
 		}
 	}
 	void OnCollisionEnter(Collision coll){
@@ -81,11 +91,59 @@ public class EnemyScript : MonoBehaviour {
 			}
 		}
 	}
-	void EngagePlayer(){
+	//Fires a bullet in the direction the enemy is facing
+	void FireWeapon(){
 		
 	}
 
 	void SearchForPlayer(){
-		
+		if (IsInLOS()) {
+			angleBeforeEngage = this.transform.rotation.y;
+			Vector3 lookAtPos = new Vector3(playerT.position.x, this.transform.position.y, playerT.position.z);
+			transform.LookAt (lookAtPos);
+			if (!firing) {
+				//InvokeRepeating();
+			}
+			firing = true;
+
+		} else if(firing){
+			firing = false;
+			this.transform.rotation = Quaternion.Euler(this.transform.rotation.x,angleBeforeEngage,this.transform.rotation.z);
+			CancelInvoke ();
+		}
+	}
+	/* checks if the player is in the Field of view
+	 * much of the code in IsInLOS and IsInFOV was obtained from http://unity.grogansoft.com/enemies-that-can-see/
+	*/
+	private bool IsInLOS(){
+		float distanceToPlayer = Vector3.Distance(transform.position, playerT.position);
+		RaycastHit[] rayHits = Physics.RaycastAll(transform.position, playerT.position - transform.position, distanceToPlayer);
+		//Used for testing: Debug.DrawRay(transform.position, playerT.position - transform.position, Color.blue);
+		foreach (RaycastHit hit in rayHits)
+		{           
+			if (hit.transform.tag != "Player")
+			{
+				return false;
+			}
+		}
+		//player is reachable if no tags were hit
+		return IsInFOV();
+	}
+
+	private bool IsInFOV(){
+		Vector3 directionToPlayer = playerT.position - transform.position; // represents the direction from the enemy to the player    
+		Debug.DrawLine(transform.position, playerT.position, Color.magenta); // a line drawn in the Scene window equivalent to directionToPlayer
+
+		Vector3 lineOfSight = this.transform.forward; // the centre of the enemy's field of view, the direction of looking directly ahead
+		Debug.DrawLine(transform.position, this.transform.forward, Color.yellow); // a line drawn in the Scene window equivalent to the enemy's field of view centre
+
+		// calculate the angle formed between the player's position and the centre of the enemy's line of sight
+		float angle = Vector3.Angle(directionToPlayer, lineOfSight);
+
+		// if the player is within 65 degrees (either direction) of the enemy's centre of vision (i.e. within a 130 degree cone whose centre is directly ahead of the enemy) return true
+		if (angle < 65)
+			return true;
+		else
+			return false;
 	}
 }
